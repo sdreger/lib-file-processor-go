fbc38812dca4cd0e57d74da6705fa68a7f8f7a16
Sergey Dreger <sergey.dreger@gmail.com>
2022-05-25 11:45:48 +0300

    Add BookMeta model for scraping

---
+++ scrapper/models.go
@@ -0,0 +1,25 @@
+package scrapper
+
+import "time"
+
+type BookMeta struct {
+	Title         string
+	SubTitle      string
+	Description   string
+	ISBN10        string
+	ISBN13        string
+	ASIN          string
+	Pages         int
+	Language      string
+	PublisherURL  string
+	Publisher     string
+	Edition       uint8
+	PubDate       time.Time
+	Authors       []string
+	Categories    []string
+	Tags          []string
+	Formats       []string
+	BookFileSize  int
+	BookFileName  string
+	CoverFileName string
+}
